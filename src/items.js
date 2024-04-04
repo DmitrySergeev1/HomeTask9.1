08b1dd33dacfdac43030f7b386dc3fb22ac9733b
DmitrySergeev1 <dmitrysergeev.dht@gmail.com>
2024-04-04 22:36:14 +0400

    first commit

---
+++ src/items.js
@@ -0,0 +1,41 @@
+export let tovarsObj=[
+    {
+        nameTovar:"Фитнес-браслет DEXP SB60",
+        imgSrc:"img\one.jpg",
+        corpus:"черный",
+        remen:"черный",
+        displaySize:"0.96\"",
+        displayType:"OLED",
+        bluetooth:true,
+        OS:{
+            os1:"Android 4.2",
+            os2:"iOS 8",
+        }
+    },
+    {
+        nameTovar:"Смарт-часы RunGo W2",
+        imgSrc:"img\two.jpg",
+        corpus:"зеленый",
+        remen:"зеленый",
+        displaySize:".3\"",
+        displayType:"TFT (TN)",
+        bluetooth: true,
+        OS:{
+            os1:"Android",
+            os2:"iOS",
+        }
+    },
+     {
+        nameTovar:"Смарт-часы Welbe Prime",
+        imgSrc:"img\three.jpg",
+        corpus:"черный",
+        remen:"розовый",
+        displaySize:"1.69\"",
+        displayType:"IPS",
+        bluetooth: true,
+        OS:{
+            os1:"Android 5.0",
+            os2:"iOS 9",
+        }
+    }
+ ]
